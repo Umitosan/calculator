e9f14802b05349ba0c3bcda35596595e83493282
Dominic Brown and Tanner Eustice <tanner@>
2017-03-20 11:14:38 -0700

    add basic calc code to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var devide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var userNumber1 = parseInt(prompt("Enter a number:"));
+var userNumber2 = parseInt(prompt("Enter another number:"));
+
+alert(devide(userNumber1, userNumber2));
